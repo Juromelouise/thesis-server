5dec59e606e7858fc04843e41f71bc97c0c8a43e
unknown <juromefernando@gmail.com>
2025-05-13 00:28:40 +0800

    api key for translate

--- service/translator.js
+++ service/translator.js
@@ -1,9 +1,12 @@
+// filepath: c:\Thesis\System\backend\service\translator.js
 const { Translate } = require("@google-cloud/translate").v2;
-const projectId = "thesis-434709";
-
-const translate = new Translate();
 
 exports.translator = async (req, res, next) => {
+  const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+// Initialize the Translate client with the API key
+const translate = new Translate({ key: apiKey });
+
   try {
     let description = req.body.description;
     const [translation] = await translate.translate(req.body.description, {
@@ -11,9 +14,7 @@ exports.translator = async (req, res, next) => {
       to: "en", // Target language (English)
     });
 
-    // Log the translation for debugging
-
-    // Update the request body with the translated description  
+    // Update the request body with the translated description
     req.body.description = {};
     req.body.description.translation = translation;
     req.body.description.original = description;
