ebbd6352c8050b888fd3b963cd63a3320ce8fc31
unknown <juromefernando@gmail.com>
2025-06-01 20:50:23 +0800

    ofcors

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const plateNumber = require("./router/plateNumber");
 const maps = require("./router/maps");
 const comment = require("./router/comment")
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+  credentials: true
+}));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 
