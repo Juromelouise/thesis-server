423b730832398711cc13bff9e855e349bff7e561
unknown <juromefernando@gmail.com>
2025-06-28 14:53:21 +0800

    obs coor heatmap

--- controller/MapController.js
+++ controller/MapController.js
@@ -1,10 +1,18 @@
 const Report = require("../model/Report");
+const Obstruction = require("../model/Obstruction");
 
 exports.getCoordinates = async (req, res) => {
-    try {
-      const coordinates = await Report.getResolvedCasesPerMonth();
-      res.status(200).json(coordinates);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  };
+  try {
+    const ReportCoordinates = await Report.getResolvedCasesPerMonth();
+    const obstructionCoordinates = await Obstruction.getResolvedCasesPerMonth();
+
+    console.log("Report Coordinates:", ReportCoordinates);
+    console.log("Obstruction Coordinates:", obstructionCoordinates);
+    
+    const coordinates = [...ReportCoordinates, ...obstructionCoordinates];
+
+    res.status(200).json(coordinates);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
